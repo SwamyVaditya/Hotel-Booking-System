9861ce40c9f7bd24094bcac781ffdabf0a851d67
NAPIER-MAIL\40214358 <40214358@live.napier.ac.uk>
2016-11-12 14:42:35 +0000

    Modified the GUIs slightly.

---
+++ assessment2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace assessment2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_addbooking_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Booking();
+            newWindow.ShowDialog();
+        }
+
+        private void btn_addcustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Customer();
+            newWindow.ShowDialog();
+        }
+
+        private void btn_invoice_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Invoice();
+            newWindow.ShowDialog();
+        }
+    }
+}
