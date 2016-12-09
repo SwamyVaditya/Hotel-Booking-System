45c7133e997fac8d7673ac92cc29285a0693b2b0
NAPIER-MAIL\40214358 <40214358@live.napier.ac.uk>
2016-12-09 21:39:37 +0000

    I have commented the invoice window.

--- assessment2/Invoice.xaml.cs
+++ assessment2/Invoice.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+//David Gibson
+//this class displays cost information for the booking
+//09/12/2016
 namespace assessment2
 {
     /// <summary>
@@ -22,43 +25,47 @@ namespace assessment2
         public Invoice(Booking booking)
         {
             InitializeComponent();
-            
+            //declare variables to hold information about the booking costs and time stayed/used extras
             double Nights = (booking.DepartureDate-booking.ArrivalDate).TotalDays;
             int ratePerNight = 0;
             double extrasCost = 0;
             double costNoExtras = 0;
             double hireDays = (booking.ListOfExtras[0].HireEndDate - booking.ListOfExtras[0].HireStartDate).TotalDays;
 
+            //if the user has hired a car then add the cost of the hire to extras 
             if (booking.ListOfExtras[0].CarHire == true)
             {
-
                 extrasCost = extrasCost + (50 * hireDays);
             }
-
-
+            //for every guest that is a part of booking
             foreach (Guest g in booking.ListOfGuests)
             {
                 if (g.age>18)
                 {
-
+                    //if the guest is over 18 then set the appropriate rate
                     ratePerNight = 50;
                 }
                 else
                 {
+                    //if the guest is under 18 then set the appropriate rate
                     ratePerNight = 30;
                 }
-
+                //set the cost per person
                 double CostPerPerson = Nights * ratePerNight;
+                //set the cost without the extras
                 costNoExtras += CostPerPerson;
+                //if the user has opted in for evening meals then add the cost of the meal to extras 
                 if (booking.ListOfExtras[0].EveningMeal == true)
                 {
                     extrasCost = extrasCost + 15 * Nights;
                 }
+                //if the user has opted in for breakfasts then add the cost of the meal to extras 
                 if (booking.ListOfExtras[0].Breakfast == true)
                 {
                     extrasCost = extrasCost + 5 * Nights;
                 }
             }
+            //Set the labels to their corresponding values
             CostPerNight.Content = "£" + costNoExtras / Nights;
             CostOfExtras.Content = "£" + extrasCost;
             TotalCost.Content = "£" + (costNoExtras + extrasCost);
