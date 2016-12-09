4c1d6b15e0e9e769aeeac5a5bc8894bc3f06357f
NAPIER-MAIL\40214358 <40214358@live.napier.ac.uk>
2016-12-09 23:27:26 +0000

    I have commented my entire project.

--- assessment2/CustomerWindow.xaml.cs
+++ assessment2/CustomerWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+//David Gibson
+//This class takes care of adding and editing the customers.
+//09/12/2016
+
 namespace assessment2
 {
     /// <summary>
@@ -19,7 +23,7 @@ namespace assessment2
     /// </summary>
     public partial class CustomerWindow : Window
     {
-        
+        //using a mainwindow here so that I can access main window throughout this class
         private MainWindow window;
         bool edit;
         Customer customer;
@@ -28,6 +32,7 @@ namespace assessment2
         {
             InitializeComponent();
             this.window = window;
+            //set the edit boolean to false 
             edit = false;
         }
         private List<Customer> customerlist = new List<Customer>();
@@ -36,24 +41,30 @@ namespace assessment2
         {
             InitializeComponent();
             this.window = window;
+            //set the edit boolean to true
             edit = true;
             this.customer = customer;
+            //load appropriate values into the text boxes
             txt_name.Text = customer.Name;
             txt_address.Text = customer.Address;
         }
 
         
-
+        //when the cancel button is clicked then close the customer window 
         private void btn_Cancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
        
+        //if the add customer button is clicked 
         private void btn_addCustomer_Click(object sender, RoutedEventArgs e)
         {
+            //if adding as opposed to editing 
             if (!edit)
             {
+                //create a new customer
                 Customer newCustomer = new Customer();
+                //set the appropriate values
                 try
                 {
                     newCustomer.Name = txt_name.Text;
@@ -64,15 +75,18 @@ namespace assessment2
                     MessageBox.Show("An error has occured: " + ntblnk.Message);
                     return;
                 }
+                //get the customer number from the factory method getcustomerno
                 newCustomer.CustomerRef = window.idFactory.GetCustomerNo();
+                //add customer to the list
                 window.customerlist.Add(newCustomer);
+                //add customer to the listview
                 window.addcustomer(newCustomer);
                 // window.updateCustomerList();
                 this.Close();
             }
             else
             {
-
+                //set the appropriate values
                 try
                 {
                     customer.Name = txt_name.Text;
@@ -83,13 +97,14 @@ namespace assessment2
                     MessageBox.Show("An error has occured: " + ntblnk.Message);
                     return;
                 }
+                //identify customer by index
                 int index = window.customerlist.FindIndex(x => x.CustomerRef == customer.CustomerRef);
+                //update the values for the corresponding customer
                 window.customerlist[index] = customer;
                 window.updateCustomerList();
+                //close the window
                 this.Close();
             }
-
-
         }
     }
 }
